9ed05649d52ecc4009a6d95a62a54e8abdd1c5a1
iiiteam11801 <iiiteam11801@gmail.com>
2021-11-19 15:37:30 +0800

    bg image

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
 
 <template>
   <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold leading-tight text-gray-900">
-          {{ $route.meta.title }} / {{ counter }}
-        </h1>
-      </div>
-    </header>
     <main>
       <router-view />
     </main>
